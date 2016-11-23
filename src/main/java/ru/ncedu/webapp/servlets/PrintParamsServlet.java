e9e1b70ead7c48ff0c1e2bc124d6f1bb6cd141f7
AndrewKozyrin <kozyrin.ai@gmail.com>
2016-11-23 09:40:51 +0400

    Modify PrintParamsServlet. Added method result,  deleted all if{}. Added annotations WebServlet. Added directory view, style and moved from him jsp and css files.

--- src/main/java/ru/ncedu/webapp/servlets/PrintParamsServlet.java
+++ src/main/java/ru/ncedu/webapp/servlets/PrintParamsServlet.java
@@ -1,11 +1,13 @@
 package ru.ncedu.webapp.servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "calc", urlPatterns = "/calc")
 public class PrintParamsServlet extends HttpServlet {
 
     @Override
@@ -23,28 +25,10 @@ public class PrintParamsServlet extends HttpServlet {
 
             double num1 = Double.parseDouble(calcRequest.getParameter("text1"));
             double num2 = Double.parseDouble(calcRequest.getParameter("text2"));
+            String operation = String.valueOf(calcRequest.getParameter("operation"));
             double result = 0;
-
-            if (calcRequest.getParameter("sum") != null) {
-                result = num1 + num2;
-            }
-            if (calcRequest.getParameter("dif") != null) {
-                result = num1 - num2;
-            }
-            if (calcRequest.getParameter("mul") != null) {
-                result = num1 * num2;
-            }
-            if (calcRequest.getParameter("div") != null) {
-                result = num1 / num2;
-            }
-            if (calcRequest.getParameter("pow") != null) {
-                result = Math.pow(num1, num2);
-            }
-            if (calcRequest.getParameter("sqrt") != null) {
-                result = Math.sqrt(num1);
-            }
-            calcRequest.setAttribute("result", result);
-            calcRequest.getRequestDispatcher("/result.jsp").forward(calcRequest, calcResponse);
+            calcRequest.setAttribute("result", result(num1, num2 ,result, operation));
+            calcRequest.getRequestDispatcher("view/result.jsp").forward(calcRequest, calcResponse);
         } catch (NumberFormatException e) {
             String error = "Number is empty";
             errors(error, calcRequest, calcResponse);
@@ -56,8 +40,32 @@ public class PrintParamsServlet extends HttpServlet {
 
     }
 
-    private void errors(String error ,HttpServletRequest errRequest, HttpServletResponse errResponse) throws ServletException, IOException {
+    private void errors(String error, HttpServletRequest errRequest, HttpServletResponse errResponse) throws ServletException, IOException {
         errRequest.setAttribute("errors", error);
-        errRequest.getRequestDispatcher("/errors.jsp").forward(errRequest, errResponse);
+        errRequest.getRequestDispatcher("view/errors.jsp").forward(errRequest, errResponse);
+    }
+
+    private double result(double num1, double num2, double result, String operation){
+        switch (operation) {
+            case "sum":
+                result = num1 + num2;
+                break;
+            case "dif":
+                result = num1 - num2;
+                break;
+            case "mul":
+                result = num1 * num2;
+                break;
+            case "div":
+                result = num1 / num2;
+                break;
+            case "pow":
+                result = Math.pow(num1, num2);
+                break;
+            case "sqrt":
+                result = Math.sqrt(num1);
+                break;
+        }
+        return result;
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,9 +1,9 @@
 <html>
 <body>
 <h2>Calculator</h2>
-<form action="printParams" method="get">
-    <link rel="nav" type="text/css" href="/main.css" media="screen" />
-    <link rel="button" type="text/css" href="/button.css" media="screen" />
+<form action="/calc" method="post">
+    <link rel="nav" type="text/css" href="/style/nav.css" media="screen" />
+    <link rel="button" type="text/css" href="/style/button.css" media="screen" />
     <label>First number (a)</label>
     <input type="text" name="text1" value="0"/>
     <br/>
@@ -16,12 +16,12 @@
     <br/>
     <ul id=nav>
         <li>
-        <input type="submit" name="sum" value="+"/>
-        <input type="submit" name="dif" value="-"/>
-        <input type="submit" name="mul" value="*"/>
-        <input type="submit" name="div" value="/"/>
-        <input type="submit" name="pow" value="a^b"/>
-        <input type="submit" name="sqrt" value="&radic;"/>
+        <button type="submit" name="operation" value="sum">+</button>
+        <button type="submit" name="operation" value="dif">-</button>
+        <button type="submit" name="operation" value="mul">*</button>
+        <button type="submit" name="operation" value="div">/</button>
+        <button type="submit" name="operation" value="pow">a^b</button>
+        <button type="submit" name="operation" value="sqrt">&radic;a</button>
         </li>
     </ul>
 
