3f8e99aec4c2c914019a4067bbb4fcf7d35930f7
Rita Loginova <ritalog10@gmail.com>
2016-11-25 12:29:47 +0400

    added functions: addition, division and multiplication

--- src/main/java/ru/ncedu/webapp/servlets/PrintParamsServlet.java
+++ src/main/java/ru/ncedu/webapp/servlets/PrintParamsServlet.java
@@ -22,14 +22,29 @@ public class PrintParamsServlet extends HttpServlet {
     }
 
     private void process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        double result = 0;
         try {
             double number1 = Double.parseDouble(request.getParameter("number1"));
             double number2 = Double.parseDouble(request.getParameter("number2"));
-            double result = minus(number1, number2);
+            int operation = Integer.valueOf(request.getParameter("operation"));
+
+            switch (operation) {
+                case 1:
+                    result = number1 + number2;
+                    break;
+                case 2:
+                    result = number1 - number2;
+                    break;
+                case 3:
+                    result = number1 * number2;
+                    break;
+                case 4:
+                    result = number1 / number2;
+                    break;
+            }
+
             request.setAttribute("Result", result);
-
             request.getRequestDispatcher("/result.jsp").forward(request, response);
-
 
         } catch (ServletException e) {
             e.printStackTrace();
@@ -39,15 +54,9 @@ public class PrintParamsServlet extends HttpServlet {
 
         } catch (NumberFormatException e) {
             e.printStackTrace();
-
             String error = "NumberFormatException";
             request.setAttribute("NumberFormatException", error);
             request.getRequestDispatcher("/numberFormatException.jsp").forward(request, response);
-
         }
-    }
-
-    private double minus(double number1, double number2) {
-        return number1 - number2;
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,10 +3,17 @@
         <form action="/calc" method="post">
 
             <input type="text" name="number1"/>
-            -
+            <br/>
+            <br/>
             <input type="text" name="number2"/>
             <br/>
-            <button type="submit">Solve</button>
+            <br/>
+            <button type="submit" name="operation" value="1">+</button>
+            <button type="submit" name="operation" value="2">-</button>
+            <br/>
+            <button type="submit" name="operation" value="3">*</button>
+            <button type="submit" name="operation" value="4">/</button>
+            <br/>
             <button type="reset">Clear</button>
         </form>
     </body>
